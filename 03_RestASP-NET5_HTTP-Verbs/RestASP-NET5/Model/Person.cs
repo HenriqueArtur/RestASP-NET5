735294768759469f287c5709ae88d553c83bd4e4
Henrique Artur Cordeiro Gomes <henriqueacg@gmail.com>
2021-02-08 14:32:33 -0300

    simple test api

--- 03_RestASP-NET5_HTTP-Verbs/RestASP-NET5/Model/Person.cs
+++ 03_RestASP-NET5_HTTP-Verbs/RestASP-NET5/Model/Person.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RestASP_NET5.Model
+﻿namespace RestASP_NET5.Model
 {
     public class Person
     {
--- 03_RestASP-NET5_HTTP-Verbs/RestASP-NET5/Startup.cs
+++ 03_RestASP-NET5_HTTP-Verbs/RestASP-NET5/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using RestASP_NET5.Services;
+using RestASP_NET5.Services.Implemetations;
 
 namespace RestASP_NET5
 {
@@ -28,10 +22,9 @@ namespace RestASP_NET5
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestASP_NET5", Version = "v1" });
-            });
+
+            // Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +33,6 @@ namespace RestASP_NET5
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestASP_NET5 v1"));
             }
 
             app.UseHttpsRedirection();
