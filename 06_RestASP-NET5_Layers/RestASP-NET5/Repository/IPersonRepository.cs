8f2d1aedd14f15e24e71a6576b16f9cb6496393e
Henrique Artur Cordeiro Gomes <henriqueacg@gmail.com>
2021-02-11 12:48:16 -0300

    layers

---
+++ 06_RestASP-NET5_Layers/RestASP-NET5/Repository/IPersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using RestASP_NET5.Model;
+using System.Collections.Generic;
+
+namespace RestASP_NET5.Repositoy
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
---
+++ 06_RestASP-NET5_Layers/RestASP-NET5/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestASP_NET5.Business;
+using RestASP_NET5.Business.Implemetations;
+using RestASP_NET5.Model.Context;
+using RestASP_NET5.Repositoy;
+using RestASP_NET5.Repositoy.Implemetations;
+
+namespace RestASP_NET5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            services.AddApiVersioning();
+
+            // Dependency Injection
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
